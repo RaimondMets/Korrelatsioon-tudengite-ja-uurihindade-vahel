0b806125d2c9daff8b7689277b140bccb949a1ad
RaimondMets <raimondmets11@gmail.com>
2025-07-09 15:48:42 +0300

    Update Korteri üürihindade keskmise graafik.py

--- Korteri üürihindade keskmise graafik.py
+++ Korteri üürihindade keskmise graafik.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 
 df = pd.read_excel("C:\\TudengiteInfo\\akadeemilised_keskmised.xlsx")
 
-print(df.columns)
+print(df.columns) #selleks, et näha, mis veerud on, et saaks graafiku koostada
 
-#joonistame graafiku
+#joonistame graafiku, hetkel tartu kohta.
 plt.figure(figsize = (12,8))
-plt.plot(df['Akadeemiline_aasta'], df['Tartu €/m²'], marker='o')
-plt.title("Keskmine üürihind ruutmeetri kohta Tartus")
+#df['Tartu €/m²'], marker='o') #Tallinna kohta tegemiseks tuleb df['Tartu €/m²'] asendada df['Tallinn €/m²']
+plt.plot(df['Akadeemiline_aasta']
+plt.title("Keskmine üürihind ruutmeetri kohta Tartus") #Pealkiri samuti, kui teha Tallinna kohta
 plt.xlabel("Akadeemiline aasta")
 plt.ylabel("Keskmine üürihind")
 plt.grid(True)
